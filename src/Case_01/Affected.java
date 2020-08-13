95168a0eb4fbd4b687d8da72525c246318c9fe01
HP <HP@LAPTOP-8DT1QFKL>
2020-08-14 00:41:00 +0530

    Implement stge

---
+++ src/Case_01/Affected.java
@@ -0,0 +1,13 @@
+package Case_01;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Affected 
+{
+   @Test()
+   public void test01()
+   {
+	   Reporter.log("hello",true);
+   }
+}
